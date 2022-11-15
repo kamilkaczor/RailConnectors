0d6c9d6a208e5350c0b70ad8bc15e8485064026f
kamilkaczor <kaczor.kamil@gmail.com>
2022-11-15 10:48:53 +0100

    Delete test1 directory

--- test1/main.py
+++
@@ -1,7 +0,0 @@
-from math import sqrt
-def find_roots(a, b, c):
-    ing = sqrt(b*b-4*a*c)
-    root_1 = (-b+ing)/(2*a)
-    root_2 = (-b-ing)/(2*a)
-    return (root_1, root_2)
-print(find_roots(2, 10, 8));
