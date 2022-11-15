0bc3c7267c01777f494ce35f62bdd2bfedd03c96
kamilkaczor <kaczor.kamil@gmail.com>
2022-11-15 10:48:45 +0100

    Delete HomeAutoBinder directory

--- HomeAutoBinder/binder/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HomeAutoBinder.settings')
-
-app = Celery('binder', broker='redis://cache:6379/0')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
